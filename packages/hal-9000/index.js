2c84c8302ec067a38e5b2528982578c03b5d9e87
kevin314 <kevlin3141@gmail.com>
2020-05-17 17:55:39 -0500

    Add global functions for hal9000

---
+++ packages/hal-9000/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  executeMenu: async function() {},
+  createMenu: function() {},
+  executeReactMenu: async function() {},
+  createReactMenu: function() {},
+  executeCheckBoxReactMenu: async function() {},
+  createCheckBoxReactMenu: function() {},
+  Bot: require('./Bot'),
+  Command: require('./Command'),
+};
